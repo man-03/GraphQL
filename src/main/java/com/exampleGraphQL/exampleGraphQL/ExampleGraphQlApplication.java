cf497310f4b858199a4a3b4a39888b9f919287cb
Manoj S (G10X, IND) <manoj.kumar+tjcorp@g10x.com>
2026-05-05 16:55:30 +0530

    graphql initial setup

---
+++ src/main/java/com/exampleGraphQL/exampleGraphQL/ExampleGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.exampleGraphQL.exampleGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleGraphQL/exampleGraphQL/ExampleGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleGraphQL.exampleGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
